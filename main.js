e851c89b2780dd5d8fca979f2b19abd41ac6f315
Geovanni Baggio F <151760853+Geova135912@users.noreply.github.com>
2025-06-02 18:03:49 -0300

    .

--- main.js
+++ main.js
@@ -3,25 +3,24 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
-    }
+    };
 }
 
+
+const agora = new Date();
+const diasEmMs = 400 * 24 * 60 * 60 * 1000; 
+const tempoObjetivo = new Date(agora.getTime() + diasEmMs);
+
+
+const tempos = [tempoObjetivo, tempoObjetivo, tempoObjetivo, tempoObjetivo];
+
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
-
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -34,19 +33,21 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [300, 20, 10, 0];
+        return [0, 0, 0, 0];
     }
 }
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+        const [dias, horas, minutos, segundos] = calculaTempo(tempos[i]);
+        document.getElementById("dias" + i).textContent = dias;
+        document.getElementById("horas" + i).textContent = horas;
+        document.getElementById("min" + i).textContent = minutos;
+        document.getElementById("seg" + i).textContent = segundos;
     }
 }
 
